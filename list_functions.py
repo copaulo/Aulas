e4eaf5bc0fae722806cd74f9b336fcc595d63e79
Raphael Dinelli <raphael.dinelli14@gmail.com>
2015-10-08 00:15:23 -0300

    Create list_functions.py

---
+++ list_functions.py
@@ -0,0 +1,11 @@
+def sum_list(x, y): 
+    return int(''.join(x))+int(''.join(y))
+    
+def min_list(x, y):
+    return int(''.join(x))-int(''.join(y))
+    
+def mult_list(x, y):
+    return int(''.join(x))*int(''.join(y))
+    
+def div_list(x, y):
+    return int(int(''.join(x))/int(''.join(y)))
