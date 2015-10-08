fe30c1c683c49c99f85dce81d3db2cb87c73fbb7
Raphael Dinelli <raphael.dinelli14@gmail.com>
2015-10-08 00:16:03 -0300

    Update list_functions.py

--- list_functions.py
+++ list_functions.py
@@ -1,11 +1,7 @@
-def sum_list(x, y): 
-    return int(''.join(x))+int(''.join(y))
+def sum_list(x, y): return int(''.join(x))+int(''.join(y))
     
-def min_list(x, y):
-    return int(''.join(x))-int(''.join(y))
+def min_list(x, y): return int(''.join(x))-int(''.join(y))
     
-def mult_list(x, y):
-    return int(''.join(x))*int(''.join(y))
+def mult_list(x, y): return int(''.join(x))*int(''.join(y))
     
-def div_list(x, y):
-    return int(int(''.join(x))/int(''.join(y)))
+def div_list(x, y): return int(int(''.join(x))/int(''.join(y)))
